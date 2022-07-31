76a468f130e5012dd0883f0893d26516f10891a3
Juan Manuel Grajales Urquijo <juan.grajalesu@gmail.com>
2022-07-31 14:11:25 -0500

    Add getProduct endpoint

--- src/app.js
+++ src/app.js
@@ -1,13 +1,19 @@
 import express from "express";
 import morgan from "morgan";
-import 'dotenv/config';
+import config from "./config";
+
+//Routes
+import productsRoutes from "./routes/products.routes";
 
 const app = express();
 
 //settings
-app.set("port", process.env.PORT);
+app.set("port", config.port);
 
 //Middlewares
 app.use(morgan("dev"));
+
+//Routes
+app.use("/api/products", productsRoutes);
 
 export default app;
