d69200e5581bd2ee6ff6a90029825169c0fda906
Juan Manuel Grajales Urquijo <juan.grajalesu@gmail.com>
2022-07-31 14:12:51 -0500

    Add database connection

---
+++ src/database/database.js
@@ -0,0 +1,10 @@
+import mysql from "promise-mysql";
+import config from "../config";
+
+const connection = mysql.createPool(config.db);
+
+function getConnection() {
+  return connection;
+}
+
+export { getConnection };
