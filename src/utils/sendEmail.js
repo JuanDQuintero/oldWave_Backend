8f2c239df6f26080e833ed3aeed5787e06310886
Carlos Quiros <carlosquiros12@gmail.com>
2022-10-29 05:08:26 -0500

    Se limpio el codigo de funciones tipo console

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -57,13 +57,6 @@ export const sendEmail = async (dataToSendEmail, email) => {
       subject: "Recibo de compra", 
       text: "", 
       html: plantilla, 
-    },
-    function (err, data) {
-      if (err) {
-        console.log("Error Occurs", err);
-      } else {
-        console.log("Email sent successfully");
-      }
     }
   );
 };
